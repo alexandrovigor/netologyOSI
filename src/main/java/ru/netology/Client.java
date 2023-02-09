175222d4baa2da84b4651804ef9236918a117017
alexandrovigor <gsandy67@mail.ru>
2023-02-09 03:45:29 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Client.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 8067;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Игорь");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+
+}
